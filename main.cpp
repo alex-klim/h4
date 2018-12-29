3d020da44e2475eaa5561e65012f40fc8700b36e
Oleksandr Klymenko <olek.klymenko@gmail.com>
2018-12-29 14:40:14 +0200

    change test in main

--- main.cpp
+++ main.cpp
@@ -6,22 +6,22 @@ int main() {
     Mystr n = " and ";
     Mystr three = "random string passing by";
 
-    std::cout << "one plus two: " << one+n+two << '\n'
-        << "Is 'one' bigger than ' and '? " << one.compare(n) << '\n';
+    std::cout << "['one' plus ' and ' plus 'two']: " << one+n+two << '\n'
+        << "[Is 'one' bigger than ' and '?]: " << one.compare(n) << '\n';
     one.resize(10);
-    std::cout << "'two' length: " << two.length() << '\n';
+    std::cout << "['two' length]: " << two.length() << '\n';
 
     Mystr four = "randomstring";
-    std::cout << "Four: " << four << '\n';
+    std::cout << "[Four]: " << four << '\n';
     Mystr five;
-    std::cout << "Enter string:\n";
+    std::cout << "[Enter string]:\n";
     std::cin >> five;
-    std::cout << "FIVE: " << five << '\n';
-    four.insert(five, 15);
-    std::cout << "fifth string inserted in fourth: " << four << '\n';
+    std::cout << "[FIVE]: " << five << '\n';
+    four.insert(five, 3);
+    std::cout << "[fifth string inserted in fourth]: " << four << '\n';
     //Mystr::swap(three, five);
-    std::cout << "position of ->" << five << "<- in " << four << ": " << four.substr(five) << '\n';
+    std::cout << "[position of ->" << five << "<- in] " << four << ": " << four.substr(five) << '\n';
     five.clear();
-    std::cout << "Cleared five: " << five << '\n';
+    std::cout << "[Cleared five]: " << five << '\n';
     return 0;
 }
